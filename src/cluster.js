fdd571eae216c4ffa26c1f513dca5397f6f3be21
Ewout Stortenbeker <4ewout@gmail.com>
2021-12-15 12:24:17 +0100

    Updated cluster example / test

--- src/cluster.js
+++ src/cluster.js
@@ -3,6 +3,15 @@ const { AceBaseServer } = require('./acebase-server');
 const cluster = require('cluster');
 const numCPUs = 2; //require('os').cpus().length;
 
+/**
+ * Node.js cluster example. You can use this as a reference how to create an AceBaseServer cluster using Node.js'
+ * built-in cluster functionality.
+ * 
+ * If you want to run a pm2 or cloud-based cluster instead, do NOT use this code. 
+ * To do that, see https://github.com/appy-one/acebase-ipc-server
+ * 
+ */
+
 let dbname = "default";
 let options = { /* default options */ }; // Load from cluster.config.js!
 options.authentication = { enabled: false };
@@ -10,52 +19,42 @@ options.https = { enabled: false };
 
 if (cluster.isMaster) {
     // Startup master
-    console.log(`Master ${process.pid} is running`);
-
-    options.cluster = {
-        enabled: true,
-        isMaster: true,
-        workers: []
-    };
-
-    for (let i = 0; i < numCPUs; i++) {
-        let worker = cluster.fork();
-        worker.on("disconnect", (worker, code, signal) => {
-            console.error(`worker ${worker.process.pid} disconnected`);
-            // TODO: restart!
-        });
-        options.cluster.workers.push(worker);
-    }
-
-    process.on("unhandledRejection", (reason, p) => {
-        console.log("Unhandled Rejection in master ", process.pid, " at: ", reason.stack);
-    });
-
-    console.log(`Starting database server with ${options.cluster.workers.length} workers`);
+    console.log(`Starting database server cluster with ${numCPUs} workers`);
     const master = new AceBase(dbname, options);
-    master.once("ready", () => {
+    master.once('ready', () => {
         console.log(`Master database server started on process ${process.pid}`);
     });
 
-    cluster.on("exit", (worker, code, signal) => {
+    for (let i = 0; i < numCPUs; i++) {
+        const worker = cluster.fork();
+        worker.on('disconnect', () => {
+            console.error(`worker ${worker.process.pid} disconnected`);
+        });
+        worker.on('message', (msg) => {
+            if (msg === 'acebase-server-shutdown') {
+                // We could handle shutdown message here to kill the worker safely, but we'll do it in the worker threads instead: see below
+                // worker.kill();
+            }
+        });
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
         console.error(`worker ${worker.process.pid} died`);
     });
 }
 else {
     console.log(`Worker ${process.pid} is running`);
 
-    options.cluster = {
-        enabled: true,
-        isMaster: false,
-        master: process
-    };
-    process.on("unhandledRejection", (reason, p) => {
-        console.log("Unhandled Rejection in worker ", process.pid, " at: ", reason.stack);
-    });
-
-    const workerServer = new AceBaseServer(dbname, options);
-    workerServer.ready(() => {
+    const server = new AceBaseServer(dbname, options);
+    server.ready(() => {
         console.log(`Worker database server started on process ${process.pid}`);
     });
-
+    // Handle shutdown event to exit the worker process safely
+    server.on('shutdown', () => {
+        process.exit();
+    });
+    // Other option:
+    // process.on('acebase-server-shutdown', () => {
+    //     process.exit();
+    // });
 }
