21769abd52c48d551615fc7427fd7ea6c719b0ac
Ewout Stortenbeker <4ewout@gmail.com>
2022-04-07 18:20:54 +0200

    moved to /dist

--- src/index.js
+++
@@ -1,2 +0,0 @@
-const { AceBaseServer, AceBaseServerSettings, AceBaseServerHttpsSettings, AceBaseServerAuthenticationSettings } = require('./acebase-server');
-module.exports = { AceBaseServer, AceBaseServerSettings, AceBaseServerHttpsSettings, AceBaseServerAuthenticationSettings };
