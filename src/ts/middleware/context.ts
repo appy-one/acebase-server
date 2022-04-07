f8ec0d62cc8aeffc04007808a527996dca6c2ac5
Ewout Stortenbeker <4ewout@gmail.com>
2022-04-07 16:28:12 +0200

    add default context to requests

--- src/ts/middleware/context.ts
+++ src/ts/middleware/context.ts
@@ -5,14 +5,16 @@ export const addMiddleware = (env: RouteInitEnvironment) => {
     env.app.use((req: RouteRequest, res, next) => {
         // Setup AceBase context, to allow clients to pass contextual info with data updates,
         // that will be sent along to data event subscribers on affected data.
-        const context = req.get('AceBase-Context');
+
+        const context = req.get('AceBase-Context') || '{}';
         try {
-            req.context = context && JSON.parse(context);
+            req.context = JSON.parse(context);
         }
         catch(err) {
             env.debug.error(`Failed to parse AceBase-Context header: "${context}" in request at ${req.url} from client ${req.ip}`);
+            return res.status(500).end('Invalid request context');
         }
-        // console.log(`Received request on path ` + req.path);
+
         next();
     });
 
