468c3415d33df59da2a68817d27bad4007aafb13
Ewout Stortenbeker <4ewout@gmail.com>
2022-05-20 14:45:49 +0200

    Exporting email and OAuthProvder types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { AceBaseServer } from './server';
 export { AceBaseServerSettings, AceBaseServerHttpsSettings, AceBaseServerTransactionSettings, AceBaseServerAuthenticationSettings } from './settings';
+export * from './shared/email';
+export * from './oauth-providers';
