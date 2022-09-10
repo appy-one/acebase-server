e4e6e9e2c903e7172d75c65d9ebb90766d4522e4
Ewout Stortenbeker <me@appy.one>
2022-09-10 11:53:42 +0200

    Fix ESM rootpath (#29)
    * Fix ESM rootpath issue
    
    * chore: build

--- src/shared/rootpath.ts
+++ src/shared/rootpath.ts
@@ -9,6 +9,8 @@ if (process.platform === 'win32' && currentDir.startsWith('/')) {
     // tsc-esm-fix does not handle win32 file urls correctly, the drive letter in import.meta.url is prefixed with a slash: file:///C:/dir/file.js
     currentDir = currentDir.slice(1);
 }
+// tsc-esm-fix also does not use decodeURI to remove encoded characters (such as %20 for spaces) 
+currentDir = decodeURI(currentDir);
 
 export const packageRootPath = resolve(currentDir, '../../..');
 
