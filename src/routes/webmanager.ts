4bb455759648435e54869ad83aeb81a905e2151c
Ewout Stortenbeker <me@appy.one>
2022-12-12 23:34:16 +0100

    use `rootPath` without slashes

--- src/routes/webmanager.ts
+++ src/routes/webmanager.ts
@@ -4,16 +4,16 @@ import { join as joinPaths } from 'path'
 
 export const addRoutes = (env: RouteInitEnvironment) => {
 
-    const webManagerDir = `/webmanager/`;
+    const webManagerDir = `webmanager`;
 
     // Add redirect from root to webmanager
     env.app.get('/', (req, res) => {
-        res.redirect(env.rootPath + webManagerDir.slice(1));
+        res.redirect(`/${env.rootPath ? `${env.rootPath}/` : ''}${webManagerDir}/`);
     });
 
     // Serve static files from webmanager directory
-    env.app.get(`${webManagerDir}*`, (req, res) => {
-        const filePath = req.path.slice(webManagerDir.length);
+    env.app.get(`/${webManagerDir}/*`, (req, res) => {
+        const filePath = req.path.slice(webManagerDir.length + 2);
         const assetsPath = joinPaths(packageRootPath, '/webmanager');
         if (filePath.length === 0) {
             // Send default file
