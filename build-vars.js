949baa7a9b0b57a5ea28a834fb9aef8c5cc8f73c
Ewout Stortenbeker <me@appy.one>
2023-01-02 23:03:33 +0100

    bind to npm version hook

--- build-vars.js
+++ build-vars.js
@@ -1,11 +1,16 @@
-// Executed by npm "prepack" script
+// Executed by npm "version" hook
 const { readFileSync, writeFileSync } = require('fs');
+const { execSync } = require('child_process');
 const packageInfo = require('./package.json');
 
 const files = ['./dist/cjs/routes/meta-info.js', './dist/esm/routes/meta-info.js'];
 files.forEach(file => {
-    let content = readFileSync(file, 'utf8');
-    content = content.replace(/%SERVER_VERSION%/g, packageInfo.version);
-    writeFileSync(file, content, 'utf8');
-    console.log(`Replaced variable %SERVER_VERSION% with ${packageInfo.version} in ${file}`);
+    const originalContent = readFileSync(file, 'utf8');
+    content = originalContent.replace(/%SERVER_VERSION%/g, packageInfo.version);
+    if (content !== originalContent) {
+        writeFileSync(file, content, 'utf8');
+        console.log(`Replaced variable %SERVER_VERSION% with ${packageInfo.version} in ${file}`);
+    }
 });
+
+execSync(`git add dist`);
