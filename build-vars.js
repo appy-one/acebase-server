90e20928b0287d86e47f49db0dc1d9b56b8b8a4d
Ewout Stortenbeker <me@appy.one>
2023-05-08 17:39:13 +0200

    Fix error update/set error types in Swagger docs, (#81)
    + auto-update server version

--- build-vars.js
+++ build-vars.js
@@ -3,7 +3,8 @@ const { readFileSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 const packageInfo = require('./package.json');
 
-const files = ['./dist/cjs/routes/meta-info.js', './dist/esm/routes/meta-info.js'];
+const sources = ['routes/meta-info', 'routes/docs'];
+const files = sources.reduce((all ,file) => (all.push(`./dist/cjs/${file}.js`, `./dist/esm/${file}.js`), all), []);
 files.forEach(file => {
     const originalContent = readFileSync(file, 'utf8');
     content = originalContent.replace(/%SERVER_VERSION%/g, packageInfo.version);
--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -18,7 +18,7 @@ export const addRoute = (env: RouteInitEnvironment) => {
             info: {
                 title: 'AceBase Server',
                 description: 'AceBase Server API endpoint documentation and test environment. This documentation is available on the server because it is running in development mode. To disable this, set your NODE_ENV environment variable to production. Many endpoints require you to authenticate using Bearer authentication. Use the _/auth/{dbname}/signin_ endpoint to obtain an access token, then click the _Authorize_ button and paste your token into the input field. For more information about AceBase, see GitHub',
-                version: '1.10.0',
+                version: '%SERVER_VERSION%', // Loaded from package.json by npm scripts
                 contact: {
                     name: 'AceBase API Support',
                     email: 'me@appy.one',
@@ -109,6 +109,13 @@ export const addRoute = (env: RouteInitEnvironment) => {
                         properties: {
                             code: { type: 'string', description: 'The string `"schema_validation_failed"`', example: 'schema_validation_failed' },
                             message: { type: 'string', description: 'The server error message', example: 'Property at path "path/property" is of the wrong type' },
+                        },
+                    },
+                    RuleValidationError: {
+                        type: 'object',
+                        properties: {
+                            code: { type: 'string', description: 'The string `"rule"`', example: 'rule' },
+                            message: { type: 'string', description: 'The server error message', example: 'write operation denied to path "path/property" by set rule' },
                         },
                     },
                     SerializedValue: {
