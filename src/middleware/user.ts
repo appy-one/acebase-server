cbc59677e46209f33b56a0233f80ff4816811d9f
Ewout Stortenbeker <me@appy.one>
2023-05-08 16:07:11 +0200

    Fix uncached user authentication

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -35,7 +35,7 @@ export const addMiddleware = (env: RouteInitEnvironment) => {
             if (!req.user) {
                 // Query database to get user for this token
                 try {
-                    await signIn({ method: 'internal', access_token: tokenDetails.access_token }, env, req);
+                    req.user = await signIn({ method: 'internal', access_token: tokenDetails.access_token }, env, req);
                 }
                 catch (err) {
                     return sendNotAuthenticatedError(res, err.code, err.message);
